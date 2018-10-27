fbf92b210c6f6295f51112f6ec6a2b4fd609abd5
Єлизавета Богачек <bohachek@ukma.edu.ua>
2018-10-28 02:25:55 +0300

    deleting fixed

--- Form1.cs
+++ Form1.cs
@@ -21,16 +21,18 @@ namespace Notes
         public Form1(string userName)
         {
             InitializeComponent();
-            sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bohachek\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30;");
+            
+            this.userName = userName;
+        
+
+            this.sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bohachek\Documents\Data.mdf;Integrated Security=True;Connect Timeout=30;");
 
             table = new DataTable();
             table.Columns.Add("TitleCol", typeof(String));
             table.Columns.Add("NoteCol", typeof(String));
 
-            this.userName = userName;
-        
 
-            SqlDataAdapter sda = new SqlDataAdapter("Select * From [tSetNotes] where username = '" + userName + "'", sqlcon);
+            sda = new SqlDataAdapter("Select title TitleCol, noteText NoteCol From [tSetNotes] where username = '" + userName + "'", sqlcon);
 
             sda.Fill(table);
 
@@ -39,15 +41,16 @@ namespace Notes
             //    dataGridView1.Columns["Message"].Visible = false;
             dataGridView1.Columns["TitleCol"].Width = 140;
 
+            sda = new SqlDataAdapter();
+
+            sqlcon.Open();
         }
 
         private void ADD_Click(object sender, EventArgs e)
         {
-            sda = new SqlDataAdapter();
 
-            sqlcon.Open();
-            sda.InsertCommand = new SqlCommand("INSERT INTO [tSetNotes] (username, title, noteText, creationDate) values( '" + userName + "', '" + textTitle.Text + "', '" + textNote.Text + "', cast('" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + "' as DATETIME))", sqlcon);
-            table.Rows.Add(textTitle.Text + "1", textNote.Text);
+            sda.InsertCommand = new SqlCommand("INSERT INTO [tSetNotes] (username, title, noteText, creationDate) values( '" + userName.ToString() + "', '" + textTitle.Text + "', '" + textNote.Text + "', cast('" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") + "' as DATETIME))", sqlcon);
+            table.Rows.Add(textTitle.Text, textNote.Text);
             sda.InsertCommand.ExecuteNonQuery();
             textTitle.Clear();
             textNote.Clear();
@@ -56,10 +59,11 @@ namespace Notes
         {
             int index = dataGridView1.CurrentCell.RowIndex;
 
-            MessageBox.Show(table.Rows[index][0].ToString());
 
-            //     sda.DeleteCommand = new SqlCommand("DELETE FROM [tSetNotes] where username =  '" + userName + "' and title = '" table.Rows[index].", sqlcon);
-            //   table.Rows[index].Delete();
+            sda.DeleteCommand = new SqlCommand("DELETE FROM [tSetNotes] where username =  '" + userName.ToString() + "' and title = '" + table.Rows[index][0].ToString() + "'", sqlcon);
+            
+            MessageBox.Show("DELETE FROM [tSetNotes] where username =  '" + userName.ToString() + "' and title = '" + table.Rows[index][0].ToString() + "'");
+            table.Rows[index].Delete();
 
         }
         private void EDIT_Click(object sender, EventArgs e)
